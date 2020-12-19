323de3392ef443c0e318c177f48b0fe2fba44dcf
Dillon Davis <07734willy@users.noreply.github.com>
2020-12-18 22:41:43 -0600

    updated colors

--- src/style.py
+++ src/style.py
@@ -8,6 +8,42 @@ class CodeStyle(Style):
 	styles = {
 		#**ColorfulStyle.styles,
 
+		Keyword: "#504834", #"#7e3a08",
+		Keyword.Constant: "#B4B03D bold",
+		String: "#B4B03D",
+		Number: "#B4B03D",
+		Operator: "#555",
+		#Operator.Word: "#999 bold",
+		Operator.Word: "#ac8854", #"#E9B770", #"#982239", #"#823b57", #"#a14222",
+		Name.Builtin: "#999 bold",
+		Name.Function: "#6C757A italic",
+		Name.Function.Magic: "bold",
+		Comment: "#555",
+	}
+
+class CodeLightStyle(Style):
+	default_style = ColorfulStyle.default_style
+	styles = {
+		#**ColorfulStyle.styles,
+
+		Keyword: "#E8CF98", #"#7e3a08",
+		Keyword.Constant: "#a09e5c bold",
+		String: "#a09e5c",
+		Number: "#a09e5c",
+		Operator: "#555",
+		#Operator.Word: "#999 bold",
+		Operator.Word: "#E9B770", #"#982239", #"#823b57", #"#a14222",
+		Name.Builtin: "#999 bold",
+		Name.Function: "#777 italic",
+		Name.Function.Magic: "bold",
+		Comment: "#555",
+	}
+
+class CodeDarkStyle(Style):
+	default_style = ColorfulStyle.default_style
+	styles = {
+		#**ColorfulStyle.styles,
+
 		Keyword: "#584314", #"#7e3a08",
 		Keyword.Constant: "#a09e5c bold",
 		String: "#a09e5c",
